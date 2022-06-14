ed73be12a1788ea2315e59268fa71e7349f12d8b
notgitika <gitijh@gmail.com>
2022-06-14 23:09:35 +0530

    two pointer ll problems

---
+++ Algorithms/DeleteTheMiddleNodeOfALinkedList.py
@@ -0,0 +1,33 @@
+"""
+You are given the head of a linked list. Delete the middle node, 
+and return the head of the modified linked list.
+
+The middle node of a linked list of size n is the ⌊n / 2⌋th node 
+from the start using 0-based indexing, where ⌊x⌋ denotes the 
+largest integer less than or equal to x.
+
+For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        prev = ListNode(0, head)
+        fast = slow = head
+        
+        if not fast.next:
+            return head.next
+        
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+            
+        prev.next = slow.next
+        
+        return head
---
+++ Algorithms/removeNthNodeFromEndofList.py
@@ -0,0 +1,30 @@
+"""
+Given the head of a linked list, 
+remove the nth node from the end of the list and return its head.
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+    
+        slow = fast = head
+        counter = 0
+        
+        for _ in range(n):
+            fast = fast.next
+            
+        if not fast:
+            return head.next
+        
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next
+            
+        slow.next = slow.next.next
+        
+        return head
